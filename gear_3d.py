5742847b093e9bd7b391d8c75d5b0feca92d9a7e
Dan <Daniel@Stanley5.com>
2024-06-25 23:28:24 -0700

    In the middle of converting 3D animation code to use matrix multiplication

--- gear_3d.py
+++ gear_3d.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from gear import Gear
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,11 +7,23 @@ import matplotlib.pyplot as plt
 
 class Gear3D(Gear):
 
+    def transform_gear_shape(self, thetas, as_):
+        r = 1
+        # 1: axis is +z, all thetas zero so far
+        x1 = r * np.sin(as_)
+        y1 = 0
+        z1 = r * np.cos(as_)
+        # 2: axis is +z, now apply thetas
+        x2 = x1 * np.cos(thetas) - y1 * np.sin(thetas)
+        y2 = x1 * np.sin(thetas) + y1 * np.cos(thetas)
+        z2 = z1
 
-    def get_plot_coords(self, center, angle):
-        #assert len(center) == 3
-        M = 4 * 5 * 7 * 4
+        # NOTE this is probably the transpose of what's normal, but we want to keep
+        # x,y,z separate on the outer axis for when we pass them to plt
+        return np.array([x2, y2, z2])
 
+    @lru_cache
+    def get_gear_shape(self, M):
         # remember that in the move from 2D to 3D, r gets replaced by a
         sample_thetas, sample_as = self.get_r_vs_theta(M)
 
@@ -20,6 +34,28 @@ class Gear3D(Gear):
         # center x coord is interpreted as angle from +z to axis in xz plane.
         # a is interpreted as angle from axis to point
         # distance from origin to point is always 1 (for now)
+
+
+    def get_gear_shape_points(self):
+
+    def get_plot_coords(self, center, angle):
+        #assert len(center) == 3
+        M = 4 * 5 * 7 * 4
+
+        ps = self.get_gear_shape(M)
+
+        axis_angle = center[0]
+        mat_rotation = np.array(([[np.cos(angle), -np.sin(angle), 0],
+                                  [np.sin(angle),  np.cos(angle), 0],
+                                  [0,             0,              1]]))
+        mat_tilt = np.array([[ np.cos(axis_angle), 0, np.sin(axis_angle)],
+                             [0,                   1, 0],
+                             [-np.sin(axis_angle), 0, np.cos(axis_angle)]])
+
+        mat = mat_tilt @ mat_rotation
+
+        xs, ys, zs = mat @ ps
+
 
         def transform(theta, a):
             # a is gear shape; contact point's angle from axis
@@ -40,6 +76,30 @@ class Gear3D(Gear):
             x1 = r * np.sin(a)
             y1 = 0
             z1 = r * np.cos(a)
+            # 2: axis is +z, rotated
+            x2 = x1 * np.cos(effective_theta) - y1 * np.sin(effective_theta)
+            y2 = x1 * np.sin(effective_theta) + y1 * np.cos(effective_theta)
+            z2 = z1
+            # 3: axis is according to axis_angle, rotated
+            x3 = x2 * np.cos(axis_angle) + z2 * np.sin(axis_angle)
+            y3 = y2
+            z3 = -x2 * np.sin(axis_angle) + z2 * np.cos(axis_angle)
+            return x3, y3, z3
+
+        def transform_mat(theta, a):
+            # a is gear shape; contact point's angle from axis
+            # theta is also gear shape, location of this point relative to reference tooth
+            # angle is the rotation of the gear about hte axis
+            # axis_angle is the rotation of the axis itself about the origin
+            axis_angle = center[0]
+            effective_theta = self.transform_thetas(theta - angle)
+
+            # # test with 2D transform
+            # x = a * np.cos(effective_theta) + axis_angle
+            # y = a * np.sin(effective_theta)
+            # z = 0
+            # return x, y, z
+
             # 2: axis is +z, rotated
             x2 = x1 * np.cos(effective_theta) - y1 * np.sin(effective_theta)
             y2 = x1 * np.sin(effective_theta) + y1 * np.cos(effective_theta)
